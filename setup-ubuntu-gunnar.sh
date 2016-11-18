5086fc217e3e484bf6c7f726e65637a2ea09c8c7
Gunnar Snorri Ragnarsson <gunnar.snorri-ragnarsson@qrtech.se>
2016-11-18 13:03:57 +0100

    apt-get -> apt and removed gajim

--- setup-ubuntu-gunnar.sh
+++ setup-ubuntu-gunnar.sh
@@ -11,11 +11,11 @@ ME=$(who | awk '{print $1}')
 
 # BASICS
 echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" > /etc/apt/sources.list.d/i3.list
-apt-get update
-apt-get --allow-unauthenticated install sur5r-keyring
-apt-get update
-apt-get dist-upgrade -y
-apt-get install vim build-essential cmake python-dev python3-dev -y
+apt update
+apt --allow-unauthenticated install sur5r-keyring
+apt update
+apt dist-upgrade -y
+apt install vim build-essential cmake python-dev python3-dev -y
 
 # vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -35,17 +35,17 @@ su -c "ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc" $ME
 su -c "ln -s $HOME/dotfiles/.i3 $HOME/.i3" $ME
 su -c "ln -s $HOME/dotfiles/.i3blocks.conf $HOME/.i3blocks.conf" $ME
 
-apt-get install i3 -y
-apt-get install fonts-font-awesome
+apt install i3 -y
+apt install fonts-font-awesome
 # Oh My Zsh
-apt-get install zsh -y
+apt install zsh -y
 chsh -s /bin/zsh $ME
 su -c 'sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"' $ME
 
 rm $HOME/.zshrc
 su -c "ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc" $ME
 
-apt-get install i3blocks -y
+apt install i3blocks -y
 su -c "git clone https://github.com/Anachron/i3blocks $HOME/i3blocks" $ME
 pushd $HOME/i3blocks/blocks
 for fullpath in /usr/share/i3blocks/*; do
@@ -55,16 +55,26 @@ popd
 
 
 # PIP
-apt-get install python-pip
+apt install python-pip
 pip install --upgrade pip
 pip install virtualenv virtualenvwrapper
 
 # VIVALDI
 wget -qO - http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
 echo "deb http://repo.vivaldi.com/stable/deb/ stable main" > /etc/apt/sources.list.d/vivaldi.list
-apt-get update
-apt-get install vivaldi-stable
+apt update
+apt install vivaldi-stable
+
+# SSH
+apt install openssh-server
+
+# UFW
+ufw enable
+ufw allow ssh
+
 
 
 # WORK SPECIFIC
-apt-get install gajim
+apt install gajim
+
+echo "Now fix NFS, Davmail, mail etc..."
